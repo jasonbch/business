2bb2a9281686229da00448faf796349138ad5647
Jason Bach <phongnghinhbach@gmail.com>
2019-06-21 03:33:17 -0700

    Updates

---
+++ precache-manifest.3d52b65e20689278aa5fa5b11e5f7792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1055ba01203dca7d5d019a901d32e6",
+    "url": "/business/index.html"
+  },
+  {
+    "revision": "cc3286a67ac74469e329",
+    "url": "/business/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "163eb09578052844af17",
+    "url": "/business/static/css/main.ff5052b4.chunk.css"
+  },
+  {
+    "revision": "cc3286a67ac74469e329",
+    "url": "/business/static/js/2.9a2e40b3.chunk.js"
+  },
+  {
+    "revision": "163eb09578052844af17",
+    "url": "/business/static/js/main.f3575c7d.chunk.js"
+  },
+  {
+    "revision": "9128552911bdf6f26a69",
+    "url": "/business/static/js/runtime~main.619436b2.js"
+  },
+  {
+    "revision": "f55b62d48569734a48d54bb609ed069e",
+    "url": "/business/static/media/bg.f55b62d4.jpg"
+  }
+]);
